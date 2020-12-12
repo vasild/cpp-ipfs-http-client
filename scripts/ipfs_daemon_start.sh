95a9cf323a52d18f22de6a89fa45368b42b35baa
Melroy van den Berg <melroy@melroy.org>
2020-12-13 00:35:59 +0100

    Fix CXX, CCXFLAGS, ..

--- scripts/ipfs_daemon_start.sh
+++ scripts/ipfs_daemon_start.sh
@@ -3,8 +3,8 @@
 "${GO_IPFS_CMD}" init
 "${GO_IPFS_CMD}" daemon > ipfs_daemon.out 2>&1 &
 while ! grep -i 'Daemon is ready' ipfs_daemon.out ; do
-  echo "$(date) Waiting for ipfs daemon to start..."
-  sleep 1
+  echo "Waiting for ipfs daemon to start..."
+  sleep 2
 done
 
 # EOF

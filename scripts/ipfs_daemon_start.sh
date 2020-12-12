bf054fdd7714d3b67877b3e1b4c6c250adb11457
Melroy van den Berg <melroy@melroy.org>
2020-12-13 00:21:54 +0100

    Testing GitHub Actions with test

--- scripts/ipfs_daemon_start.sh
+++ scripts/ipfs_daemon_start.sh
@@ -1,10 +1,7 @@
 #!/bin/sh -ex
 
-export IPFS_PATH="${TRAVIS_BUILD_DIR}/../ipfs-repository"
-
-rm -fr "${IPFS_PATH}"
-"${TRAVIS_BUILD_DIR}/../go-ipfs/go-ipfs/ipfs" init
-"${TRAVIS_BUILD_DIR}/../go-ipfs/go-ipfs/ipfs" daemon > ipfs_daemon.out 2>&1 &
+"${GO_IPFS_CMD}" init
+"${GO_IPFS_CMD}" daemon > ipfs_daemon.out 2>&1 &
 while ! grep -i 'Daemon is ready' ipfs_daemon.out ; do
   echo "$(date) Waiting for ipfs daemon to start..."
   sleep 1

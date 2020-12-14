8224e1cd01408f7972dd6fcd4c9100cb4835939e
Melroy van den Berg <melroy@melroy.org>
2020-12-14 04:15:27 +0100

    Add doxygen to CI/CD

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -2,7 +2,7 @@ find_package(Doxygen REQUIRED)
 set(DOXYGEN_CONFIG ${CMAKE_CURRENT_BINARY_DIR}/../Doxyfile.conf)
 
 # note the option ALL which allows to build the docs together with the application
-add_custom_target(Doxygen ALL
+add_custom_target(doc ALL
   COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_CONFIG}
   WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
   COMMENT "Generating API documentation with Doxygen"
